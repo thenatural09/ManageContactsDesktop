90b5b137283d3965ae9c8d7e81523cf3bcb0042c
Troy <tward4@tulane.edu>
2016-09-28 00:03:17 -0400

    Added load json

--- src/com/troyward/Controller.java
+++ src/com/troyward/Controller.java
@@ -52,6 +52,7 @@ public class Controller implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         list.setItems(contacts);
+        Main.loadJson("contacts.json");
     }
 
 }
--- src/com/troyward/Main.java
+++ src/com/troyward/Main.java
@@ -6,9 +6,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import jodd.json.JsonParser;
 import jodd.json.JsonSerializer;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 
 public class Main extends Application {
@@ -33,6 +36,22 @@ public class Main extends Application {
             fw.close();
         } catch(Exception e) {
             throw new Exception("Can't save");
+        }
+    }
+
+    public static void loadJson(String fileName) {
+        File f = new File(fileName);
+        FileReader fr = null;
+        try {
+            fr = new FileReader(f);
+            int fileSize = (int) f.length();
+            char[] contents = new char[fileSize];
+            fr.read(contents, 0, fileSize);
+            JsonParser parser = new JsonParser();
+            ContactWrapper cw = parser.parse(contents,ContactWrapper.class);
+            System.out.println(cw);
+        } catch (Exception e) {
+            System.out.println("Couldn't load file");
         }
     }
 
